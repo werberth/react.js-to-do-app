6e6260ca4b0e70e668f75ec543809a4da89e1aa1
Werberth Vinícius <werberthvinicius@gmail.com>
2018-04-03 02:58:48 -0300

    Setup frontend project and make a component hello world

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     devServer: {
         port: 8080,
-        contentase: './public'
+        contentBase: './public'
     },
     resolve: {
         extensions: ['', '.js', '.jsx'],
