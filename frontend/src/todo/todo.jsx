718123dbf4a2048e042368d89b813f2ca220a95e
Werberth Vinícius <werberthvinicius@gmail.com>
2018-04-04 11:25:07 -0300

    Add ToDo form

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
 
 export default class Todo extends Component {
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
+                <TodoForm />
             </div>
         )
     }
