59f109d1ae8157b39f8babc10633a123ae224687
Werberth Vinícius <werberthvinicius@gmail.com>
2018-04-19 01:08:36 -0300

    Add events

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -3,11 +3,21 @@ import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 
 export default class Todo extends Component {
+
+    constructor(props){
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd() {
+        console.log(this)
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd} />
             </div>
         )
     }
