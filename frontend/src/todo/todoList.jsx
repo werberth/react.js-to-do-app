2a3a289b0cf89a195f427625b4fac935a474d650
Werberth Vinícius <werberthvinicius@gmail.com>
2018-04-23 12:28:09 -0300

    Define dinamic hide property and css class to TodoList buttons

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -7,12 +7,14 @@ export default props => {
         const list = props.list || []
         return list.map(todo => (
             <tr key={todo._id}>
-                <td>{todo.description}</td>
+                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
                     <IconButton style='success' icon='check'
-                        onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+                        onClick={() => props.handleMarkAsDone(todo)}
+                        hide={todo.done}></IconButton>
                     <IconButton style='warning' icon='undo'
-                        onClick={() => props.handleMarkAsPending(todo)}></IconButton>
+                        onClick={() => props.handleMarkAsPending(todo)}
+                        hide={!todo.done}></IconButton>
                     <IconButton style='danger' icon='trash-o'
                         onClick={() => props.handleRemove(todo)}></IconButton>
                 </td>
